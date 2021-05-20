3f9e4cd9ca4ef01e74e78fb6f6e86d45a437a6b7
Daniel Gutierrez <alumno.578876@ies-azarquiel.es>
2021-05-20 23:43:43 +0200

    added sql script

---
+++ init.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE `loteria` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `surname` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `dni` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `apuestas` (
+  `apuesta_id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_date` date NOT NULL DEFAULT current_timestamp(),
+  `sorteo_date` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `combinacion` text COLLATE utf8_unicode_ci NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`apuesta_id`),
+  KEY `apuestas_FK` (`user_id`),
+  CONSTRAINT `apuestas_FK` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=92 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
